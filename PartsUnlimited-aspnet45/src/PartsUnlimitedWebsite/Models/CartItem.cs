cba97a74f7a7b926225ad54e0a37fb91e8f018e7
miGe <mige1981@hotmail.com>
2021-07-13 13:27:05 +0300

    testi 3

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        // eka testi
+        // toka testi
         [Key]
         public int CartItemId { get; set; }
 
